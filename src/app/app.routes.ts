5640dd39249530410ae467cad2ba7301919eb5d5
dacupetre <cupetre_kiko@yahoo.com>
2025-12-15 17:05:09 +0100

    routing changes and improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { PropertyListComponent } from './property-list/property-list';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
 
 export const routes: Routes = [
   { path: '', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
+  { path: 'rent-property', component: PropertyListComponent}, 
+  { path: 'detail-property/:id', component: PropertyDetailComponent}
 ];
--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-property-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css']
 })
